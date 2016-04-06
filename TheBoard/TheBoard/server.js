c8a9251c4aebc0915e426f661083bc6f485fbf55
Willian Fakri <wfakri@ciandt.com>
2016-04-06 16:07:19 -0300

    Adicionado projeto TheBoard

---
+++ TheBoard/TheBoard/server.js
@@ -0,0 +1,7 @@
+﻿var http = require('http');
+var port = process.env.port || 1337;
+http.createServer(function (req, res) {
+    //res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write("<html><header><title>TITULO MAROTO</title></header><body><h1>As arvre somos nozes.</h1></body></html>");
+    res.end();
+}).listen(port);
